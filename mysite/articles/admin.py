a198c1c77045e4b7406df597d3d280ad0a7b2f81
netComm <soyun3963@gmail.com>
2020-06-15 22:35:26 +0900

    0615 Django SQLite admin 페이지 만들기 실습

--- mysite/articles/admin.py
+++ mysite/articles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Article
 # Register your models here.
+# 모델에 대한 정의 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title','content','created_at','updated_at')
+admin.site.register(Article,ArticleAdmin)
--- mysite/articles/models.py
+++ mysite/articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model): # articles_article
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) # 자동으로 생성되는 시간 추가
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.id}번째글 - {self.title} : {self.content}'
+        
+
+
